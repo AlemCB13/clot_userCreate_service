5a2e194945fd8d7a8dac76df19fa5cafbc2cd8ab
Cristian Alemán <bladimiralemanc@gmail.com>
2025-02-08 13:52:53 -0500

    feat: :wrench: Fix some troubles on dockerfile and .yml for docker-compose and .env

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { createUser } = require("../controllers/userController");
 
-// Ruta para crear un usuario
+// User Create Route
 router.post("/", createUser);
 
 module.exports = router;
